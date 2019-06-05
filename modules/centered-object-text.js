18f947d89b29f18ffcee5de4a6989bf0fd26204f
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2019-06-04 17:04:34 -0700

    Add modules locally

---
+++ modules/centered-object-text.js
@@ -0,0 +1,42 @@
+var centeredObjectText = function(object, margin) {
+	if(typeof object !== "object") {
+		return;
+	}
+	if(!margin) {
+		margin = Object.keys(object).sort(function (a, b) {
+			return b.length - a.length;
+		})[0].length;
+	}
+	var result = "";
+	for(var key in object) {
+		if(typeof object[key] === "object") {
+			result += "\n" + (" ".repeat(margin - key.length) + key + " : " + object[key][0]);
+			for(var i = 1; i < object[key].length; i++) {
+				result += "\n" + " ".repeat(margin + 3) + object[key][i];
+			}
+		} else {
+			result += "\n" + (" ".repeat(margin - key.length) + key + " : " + object[key]);
+		}
+	}
+	return result;
+};
+
+var export_module;
+if(export_module) {
+    export_module(centeredObjectText);
+} else {
+    fill(0);
+    textFont(createFont("monospace"));
+    text(centeredObjectText({
+        "alpha": "bravo",
+        "charlie": "delta",
+        "echo": ["one", "two"],
+        "foxtrot": "gamma"
+    }), 100, 100);
+    println(centeredObjectText({
+        "alpha": "bravo",
+        "charlie": "delta",
+        "echo": ["one", "two"],
+        "foxtrot": "gamma"
+    }));
+}
---
+++ modules/highlight-text.js
@@ -0,0 +1,30 @@
+var highlightText = function(string, x, y, highlightColor) {
+	if(!(/\S/).test(string)) {
+		return;
+	}
+	x = x || 0;
+	y = y || textAscent();
+	highlightColor = highlightColor || color(255, 255, 0);
+	string = string.split("\n");
+	noStroke();
+	rectMode(CORNER);
+	textAlign(LEFT, TOP);
+	for(var i in string) {
+	    string[i] = " " + string[i] + " ";
+	    pushStyle();
+	    fill(highlightColor);
+	    rect(x, y + (i * textAscent() * 2), textWidth(string[i]), textAscent() * 1.75);
+	    popStyle();
+	    text(string[i], x, y + (i * textAscent() * 2));
+	}
+};
+
+var export_module;
+if(export_module) {
+    export_module(highlightText);
+} else {
+    var string = "Highlighted\nText";
+    fill(0);
+    textFont(createFont("monospace"), 25);
+    highlightText(string, (width / 2) - (textWidth(string) / 2), (height / 2) - textAscent() * 2);
+}
