615792be74315d59df0e2d291f564f986f305834
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-04-28 14:06:29 -0700

    Create `printMD`
    If links printed to the canvas log are clicked normally, the URL is
    opened in the canvas itself (which will result in a blank page except
    for select domains). So, links must be opened indirectly.
    
    The chosen method of accomplishing this is to use the `<abbr>` tag to
    display a "Ctrl + Click" message on mouse hover and the `target="_blank"
    property of the `<a>` tag to prevent links from being opened directly.

--- src/external/printHTML.js
+++ src/external/printHTML.js
@@ -1,14 +1,19 @@
 /**
+ * @summary
  * Prints HTML to canvas log.
+ * 
+ * @description
+ * Links must be opening while holding the CTRL key.
  * 
  * @param {string} data Text to be printed to canvas log
  * 
  * @example
  * printHTML('<marquee>Hello World!</marquee>');
  * 
- * @see $canvasLog
+ * @see printMD
  */
 printHTML = data => {
+    data = data.replace(/<a href="([\w+:/.]+)">(?<=\>)(.+?)(?=\<)<\/a>/g, `<abbr title="Ctrl + Click"><a href="$1" target="_blank">$2</a></abbr>`);
     const previousData = $(CANVAS_LOG).html();
     if (!previousData) e.println('');
     $(CANVAS_LOG).html((previousData && (previousData + '<br>') || '') + data);
---
+++ src/external/printMD.js
@@ -0,0 +1,55 @@
+/**
+ * @summary
+ * Prints markdown to canvas log.
+ * 
+ * @description
+ * Markdown features are limited to bold, italics and hyperlinks. Links must be
+ * opening while holding the CTRL key.
+ *
+ * @param {string} data Text to be printed to canvas log
+ * 
+ * @example
+ * printMD('*bold*');
+ * // expected outcome: bold text
+ * 
+ * @example
+ * printMD('_italic_');
+ * // expected oucome: italic test
+ * 
+ * @example
+ * printMD('[link](https://khanacademy.org)');
+ * // expected oucome: hyperlink to Khan Academy
+ * 
+ * @see printHTML
+ */
+printMD = data => {
+    let str = '';
+    let bold, italic;
+    function formatMD(tag, open) {
+        str += `<${open ? '/' : ''}${tag}>`;
+    }
+    for (let i = 0; i < data.length; i++) {
+        switch (data[i]) {
+            case '*':
+                formatMD('b', bold);
+                bold = !bold;
+                break;
+            case '_':
+                formatMD('i', italic);
+                italic = !italic;
+                break;
+            case '[':
+                try {
+                    const substr = data.substr(i, data.length);
+                    const text = substr.match(/(?<=\[).+?(?=\])/)[0];
+                    const URL = substr.match(/(?<=\().+?(?=\))/)[0];
+                    str += `<a href="${URL}">${text}</a>`;
+                    i += text.length + URL.length + 3;
+                    break;
+                } catch (err) { }
+            default:
+                str += data[i];
+        }
+    }
+    printHTML(str);
+}
