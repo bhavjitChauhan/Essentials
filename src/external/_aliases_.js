1032e791eed5bfc99c98859e6cd505fe49cd40dd
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-04-28 17:47:28 -0700

    Add DOM manipulation aliases

--- src/external/_aliases_.js
+++ src/external/_aliases_.js
@@ -3,6 +3,8 @@ _doc = _eval('documen' + 't');
 
 CREATE_ELEMENT = 'createElemen' + 't';
 
+_preventDefault = e => e.preventDefault();
+
 /**
  * Canvas jQuery selector.
  *
@@ -10,13 +12,71 @@ CREATE_ELEMENT = 'createElemen' + 't';
  * // Log canvas element style to browser console
  * console.log($(CANVAS).attr('style)');
  */
- CANVAS = '#output-canvas';
+CANVAS = '#output-canvas';
 
- /**
-  * Canvas log jQuery selector.
-  *
-  * @example
-  * // Log canvas log text to browser console
-  * console.log($(CANVAS_LOG).text());
-  */
- CANVAS_LOG = 'body div:first div:nth-child(2) div div';
+/**
+ * Canvas log jQuery selector.
+ *
+ * @example
+ * // Log canvas log text to browser console
+ * console.log($(CANVAS_LOG).text());
+ */
+CANVAS_LOG = 'body div:first div:nth-child(2) div div';
+
+/**
+ * Prevents page from scrolling when mouse is over canvas.
+ * 
+ * @see allowScroll
+ * @see forceAllowScroll
+ */
+preventScroll = () => $(CANVAS).on('wheel', _preventDefault);
+
+/**
+ * Allows page scrolling when mouse is over canvas.
+ * 
+ * @see forceAllowScroll
+ * @see preventScroll
+ */
+allowScroll = () => $(CANVAS).off('wheel', _preventDefault);
+
+/**
+ * @summary
+ * Removes all 'wheel' event handlers attached to the canvas.
+ * 
+ * @description
+ * `allowScroll` only removes the event handler that prevents the page from
+ * being scrolled, however if the program is editted or restarted this is no
+ * longer possible.
+ * 
+ * This function is not intended for production use.
+ * 
+ * @see allowScroll
+ * @see preventScroll
+ */
+forceAllowScroll = () => $(CANVAS).off('wheel');
+
+/**
+ * Locks pointer to canvas.
+ * 
+ * @see exitPointerLock
+ */
+requestPointerLock = () => $(CANVAS)[0].requestPointerLock();
+
+/**
+ * Releases pointer.
+ * 
+ * @see requestPointerLock
+ */
+exitPointerLock = () => _doc.exitPointerLock();
+
+/**
+ * Resizes the canvas log.
+ * 
+ * @param {number} height
+ */
+resizeLog = height => $('body > div:nth-child(1)').css('height', height);
+
+/**
+ * Closes the canvas log
+ */
+closeLog = () => $('body > div:nth-child(1) > div:nth-child(3)').click();
