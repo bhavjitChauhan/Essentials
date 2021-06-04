3bc40b8f1b1b191d03ce03bc499acd4231beeee0
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-06-03 22:16:59 -0700

    Add Canvas API functions to `push`/`pop`

---
+++ src/core/pop.js
@@ -0,0 +1,18 @@
+/**
+* Restores Processing and Canvas state.
+*
+* @example
+* push();
+* stroke(WHITE);
+* rotate(90);
+* rect(10, 10, 15, 15);
+* pop();
+* // This rectangle will not display the stroke or rotation
+* rect(10, 10, 15, 15);
+*
+* @see {@link push}
+*/
+pop = () => {
+    e.popStyle();
+    ctx.restore();
+};
---
+++ src/core/push.js
@@ -0,0 +1,18 @@
+/**
+ * Saves Processing and Canvas state.
+ *
+ * @example
+ * push();
+ * stroke(WHITE);
+ * rotate(90);
+ * rect(10, 10, 15, 15);
+ * pop();
+ * // This rectangle will not display the stroke or rotation
+ * rect(10, 10, 15, 15);
+ *
+ * @see {@link pop}
+ */
+push = () => {
+    e.pushStyle();
+    ctx.save();
+};
