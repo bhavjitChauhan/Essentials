27088c224d73d9201c6c60e3856eca562799d982
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-06-03 22:13:34 -0700

    Add External Essentials functions to `restart`

--- src/core/restart.js
+++ src/core/restart.js
@@ -1,7 +1,18 @@
 /**
+ * @summary
  * Cleanly restarts the program.
+ * 
+ * @description
+ * If External Essentials is present, the following functions are called:
+ * `clearElements`, `closeLog`, `exitPointerLock`, and `forceAllowScroll`.
  */
 restart = () => {
+    if (typeof EXTERNAL_ESSENTIALS != 'undefined') {
+        clearElements();
+        closeLog();
+        exitPointerLock();
+        forceAllowScroll();
+    }
     e.draw = noop;
     e.Program.restart();
 };
