fbe065c08ed2804e0ec2501344431a4362981e82
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-06-19 18:49:39 -0700

    Add basic colors to Essentials Core

---
+++ src/core/_colors_.js
@@ -0,0 +1,34 @@
+/**
+ * @summary
+ * Alias for `color(255, 0, 0)`.
+ *
+ * @description
+ * Essentials includes the
+ * [CSS color names]{@link https://w3schools.com/colors/colors_names.asp} in
+ * the format `COLORNAME`.
+ * 
+ * See the wiki page on
+ * [Colors]{@link https://github.com/bhavjitChauhan/Essentials/wiki/Colors}.
+ *
+ * @constant {number}
+ *
+ * @example
+ * fill(RED);
+ * text('Hello World', 25, 25);
+ *
+ * @example
+ * const col = color(RED, 50);
+ * fill(col);
+ * square(25, 25, 25);
+ */
+RED = 0xFFFF0000;
+ORANGE = 0xFFFFA500;
+YELLOW = 0xFFFFFF00;
+GREEN = 0xFF008000;
+BLUE = 0xFF0000FF;
+INDIGO = 0xFF4B0082;
+VIOLET = 0xFFEE82EE;
+
+WHITE = 0xFFFFFFFF;
+BLACK = 0xFF000000;
+TRANSPARENT = 0x00FFFFFF;
