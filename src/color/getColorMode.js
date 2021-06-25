445ddf13461fb52c5d7c43a247a7a7528feb1d19
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-06-25 15:25:12 -0700

    Fix `getColorMode` for custom color range

--- src/color/getColorMode.js
+++ src/color/getColorMode.js
@@ -6,4 +6,4 @@
  * println(getColorMode() == RGB);
  * // expected output: true
  */
-getColorMode = () => p.color(255, 0, 0) == -0x10000 ? p.RGB : p.HSB;
+getColorMode = () => p.color(getRedRange(), 0, 0) == -0x10000 ? p.RGB : p.HSB;
