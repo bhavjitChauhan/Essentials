fd302679366982f7cc271106f63b29317e23ed00
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-06-26 20:45:48 -0700

    Revamp `HSBToRGB`
    Also using a more compact conversion algorithm from Wikipedia.

--- src/color/HSBToRGB.js
+++ src/color/HSBToRGB.js
@@ -1,45 +1,43 @@
 /**
- * Converts HSB to RGB color type.
+ * Converts HSB to RGB.
+ * 
+ * @link https://stackoverflow.com/a/54070620
  *
- * @param {(number|color)} x Hue value or color
- * @param {number} [s] Saturation value
- * @param {number} [v] Brightness value
+ * @param {...number} args
  *
- * @returns {string}  RGB color value
+ * @returns {Array}
  *
  * @example
- * const col = HSBToRGB(85, 255, 255);
- * println(col);
- * // expected output: -16711936
- * background(col);
- * // expected outcome: green background
+ * println(HSBToRGB(0, 100, 100));
+ * // expected outcome: [255, 0, 0, 255]
+ * @example
+ * println(HSBToRGB(BLUE));
+ * // expected outcome: [0, 0, 255, 255]
  */
-HSBToRGB = function(x, s, v) {
-    if (arguments.length == 1) {
-        c = x;
-        x = p.hue(c), s = p.saturation(c), v = p.brightness(c);
+HSBToRGB = (...args) => {
+    const currentColorRange = getColorRange();
+    const mapColorRange = getColorMode() == p.RGB ? currentColorRange : RGB_COLOR_RANGE;
+    let h, s, b, a;
+    switch (args.length) {
+        case 1: {
+            const notDefaultColorRange = !isDefaultColorRange();
+            if (notDefaultColorRange) {
+                p.pushStyle();
+                presetColorMode(p.HSB);
+            }
+            [h, s, b, a] = toHSB(args[0]);
+            notDefaultColorRange && p.popStyle();
+            h /= 60, s /= 100, b /= 100, a /= 100;
+            break;
+        }
+        case 3:
+        case 4:
+            if (args.length == 4) a = args.pop() / HSB_COLOR_RANGE[3];
+            h = args.shift() / 60;
+            [s, b] = args.map((value, i) => value /= HSB_COLOR_RANGE[i + 1]);
     }
-    x /= 255, s /= 255, v /= 255;
-
-    const i = Math.floor(x * 6),
-        f = x * 6 - i,
-        p = v * (1 - s),
-        q = v * (1 - f * s),
-        t = v * (1 - (1 - f) * s);
-
-    let r, g, b;
-    switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-
-    const result = [r, g, b].map(function (i) {
-        return i * 255;
-    });
-
-    return p.color.apply(p, result);
+    f = (n, k = (n + h) % 6) => b - b * s * Math.max(Math.min(k, 4 - k, 1), 0);
+    let arr = [f(5), f(3), f(1), a || 1];
+    arr = arr.map((value, i) => value * mapColorRange[i]);
+    return arr;
 };
