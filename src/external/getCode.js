23a51181675780a5ed478fc51df97b4be1c2c33b
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-06-27 17:58:01 -0700

    Disallow `getCode` in event functions
    Since the event functions are not called by the Processing evaluator, they do not contain
    the editor code arguments.

--- src/external/getCode.js
+++ src/external/getCode.js
@@ -7,6 +7,7 @@ getCode = function () {
     let obj = arguments.callee;
     while (!(obj.arguments[0] instanceof Processing)) {
         obj = obj.caller;
+        if (obj.arguments[0] instanceof MouseEvent || obj.arguments[0] instanceof KeyboardEvent) return console.warn('`getCode` is not supported in Proccessing event functions.');
     }
     return obj.caller.arguments[0];
 };
