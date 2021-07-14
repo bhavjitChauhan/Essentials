d2126495bf9808d3ac462605fc380814354ce16b
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2021-07-13 17:02:53 -0700

    Fix `shadow` multiple arguments

--- src/color/shadow.js
+++ src/color/shadow.js
@@ -1,15 +1,27 @@
 /**
  * Sets shadow color.
  * 
- * @param {number|string} color
+ * @param {...number|color|string} args
  * 
  * @example
+ * push();
  * shadow(BLACK);
  * shadowBlur(10);
  * square(100, 100, 100);
+ * pop();
  * // expected outcome: square with a black shadow
+ * 
+ * @example
+ * push();
+ * shadow(255, 0, 0);
+ * shadowBlur(10);
+ * circle(100, 100, 100);
+ * pop();
+ * // expected outcome: circle with a red shadow
  */
-shadow = color => {
-    if (_.isNumber(color)) color = RGBToHex(color, false);
+shadow = (...args) => {
+    let color = args[0];
+    if (args.length > 1) color = p.color.apply(null, args);
+    if (_.isNumber(args[0])) color = RGBToHex(color, false);
     ctx.shadowColor = color;
 };
