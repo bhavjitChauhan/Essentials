7b4306cad63a75360e7fca3da027d80e082cf5af
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2022-04-18 16:24:47 -0700

    Add `canvas` alias

---
+++ src/core/aliases/canvas-api.js
@@ -0,0 +1,21 @@
+/**
+ * Alias for the
+ * [`HTMLCanvasElement`]{@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement}
+ * used by Processing.
+ * 
+ * @constant {HTMLCanvasElement}
+ * 
+ * @category Core
+ */
+canvas = p.externals.canvas;
+
+/**
+ * Alias for the
+ * [`CanvasRenderingContext2D`]{@link https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D}
+ * used by Processing.
+ * 
+ * @constant {CanvasRenderingContext2D}
+ * 
+ * @category Core
+ */
+ctx = p.externals.context;
