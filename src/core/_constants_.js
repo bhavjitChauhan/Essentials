22666e79e448e9b9da7b21d3612dbc315d70e695
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2022-04-18 16:24:26 -0700

    Convert `WIDTH`/`HEIGHT` to getters

--- src/core/_constants_.js
+++ src/core/_constants_.js
@@ -6,53 +6,6 @@ p = Processing.instances[0];
 TOP_LEFT = 1, TOP_RIGHT = 2, BOTTOM_RIGHT = 3, BOTTOM_LEFT = 4;
 
 /**
- * Alias for `width`.
- * 
- * @constant {number}
- * 
- * @category Core
- */
-WIDTH = p.width;
-
-/**
- * Alias for `height`.
- * 
- * @constant {number}
- * 
- * @category Core
- */
-HEIGHT = p.height;
-
-/**
- * Alias for `WIDTH / 2`.
- * 
- * @constant {number}
- * 
- * @category Core
- */
-HALF_WIDTH = WIDTH / 2;
-
-/**
- * Alias for `HEIGHT / 2`.
- * 
- * @constant {number}
- * 
- * @category Core
- */
-HALF_HEIGHT = HEIGHT / 2;
-
-/**
- * Alias for the
- * [`CanvasRenderingContext2D`]{@link https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D}
- * used by Processing.
- * 
- * @constant {CanvasRenderingContext2D}
- * 
- * @category Core
- */
-ctx = p.externals.context;
-
-/**
  * Literally does nothing. Useful as the default for optional callback arguments.
  * 
  * @category Core
@@ -61,4 +14,4 @@ ctx = p.externals.context;
  * // Prevents an error if an unneeded method is called
  * Element.init = noop;
  */
- noop = function () {};
+noop = function () { };
---
+++ src/core/aliases/width-height.js
@@ -0,0 +1,4 @@
+Object.defineProperty(this, 'WIDTH', { get: () => { return canvas.width; } });
+Object.defineProperty(this, 'HEIGHT', { get: () => { return canvas.height; } });
+Object.defineProperty(this, 'HALF_WIDTH', { get: () => { return canvas.width / 2; } });
+Object.defineProperty(this, 'HALF_HEIGHT', { get: () => { return canvas.height / 2; } });
