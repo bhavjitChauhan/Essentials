cc80fb0d26c16ade04f26c640659730274fbd30d
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2022-04-18 19:29:46 -0700

    Add `WIDTH`/`HEIGHT` JSDoc comments

--- src/core/aliases/width-height.js
+++ src/core/aliases/width-height.js
@@ -1,4 +1,35 @@
+/**
+ * @constant {number} WIDTH
+ * 
+ * Alias for `canvas.width`.
+ * 
+ * @category Core
+ */
 Object.defineProperty(this, 'WIDTH', { get: () => { return canvas.width; } });
+
+/**
+ * @constant {number} HEIGHT
+ * 
+ * Alias for `canvas.height`.
+ * 
+ * @category Core
+ */
 Object.defineProperty(this, 'HEIGHT', { get: () => { return canvas.height; } });
+
+/**
+ * @constant {number} HALF_WIDTH
+ * 
+ * Alias for `canvas.width / 2`.
+ * 
+ * @category Core
+ */
 Object.defineProperty(this, 'HALF_WIDTH', { get: () => { return canvas.width / 2; } });
+
+/**
+ * @constant {number} HALF_HEIGHT
+ * 
+ * Alias for `canvas.height / 2`.
+ * 
+ * @category Core
+ */
 Object.defineProperty(this, 'HALF_HEIGHT', { get: () => { return canvas.height / 2; } });
